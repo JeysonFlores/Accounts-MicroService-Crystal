4168e5915378e1a79130a6021601c4cdcb1dff75
Jeyson Flores <68255757+JeysonFlores@users.noreply.github.com>
2020-09-14 12:15:58 -0600

    Add files via upload

--- src/server.cr
+++ src/server.cr
@@ -5,6 +5,22 @@ require "jwt"
 exp = Time.utc.to_unix + 1500
 SECRET_KEY = "ABCDEFGHIjKlMntY"
 
+error 400 do 
+  {error: "1", message: "Invalid request"}.to_json
+end
+
+error 403 do 
+  {error: "1", message: "Unauthorized request"}.to_json
+end
+
+error 404 do
+  {error: "1", message: "Invalid route"}.to_json
+end
+
+error 405 do 
+  {error: "1", message: "Invalid method"}.to_json
+end
+
 post "/signup" do |env|
   response = {error: "0", message: "Sign up completed!"}.to_json
   env.response.content_type = "application/json"
